54445e999e4022696e763f51804bb2a0b0b2a8bb
Isaabellaasmit <isabellasmit.2604@gmail.com>
2023-06-13 16:45:27 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+
+// import scrollama from "scrollama";
+
+// instantiate the scrollama
+// const scroller = scrollama();
+
+// // setup the instance, pass callback functions
+// scroller
+//   .setup({
+//     step: ".step",
+//   })
+//   .onStepEnter((response) => {
+//     // { element, index, direction }
+//   })
+//   .onStepExit((response) => {
+//     // { element, index, direction }
+//   });
+
+
+
+
+
+
+// let moon = document.getElementById("maan");
+// let stars = document.getElementById("sterren");
+// let voorBerg = document.getElementById("bergen_front");
+// let achterBerg = document.getElementById("bergen_back");
+// let text = document.getElementById("text");
+
+// window.addEventListener("scroll", function(){
+//     let value = window.scrollY;
+//     stars.style.left = value * 0.25 + "px"; 
+//     moon.style.top = value * 1.05 + "px";
+//     voorBerg.style.top = value * 0 + "px";
+//     achterBerg.style.top = value * 0.5 + "px";
+//     text.style.marginRight = value * 4 + "px";
+//     text.style.marginTop = value * 1.5 + "px";
+// })
